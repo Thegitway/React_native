a4f1756fb033a1b89c18528749bc7480fc206dab
Omar <omaroukil31@gmail.com>
2021-04-11 00:47:13 +0100

    fist Commit

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button,Image, StyleSheet, Text, ToastAndroid, View } from 'react-native';
+import { Dimensions } from "react-native";
+const win = Dimensions.get('window');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.row}>
+       <Image style={styles.image} source={{uri:"./assets/icon.png"}}/>
+
+       <View >
+       <Text style ={styles.title}>Title</Text>
+       <Text style={styles.description}>Desctiption</Text>
+       </View>
+       <View style={{flex:1, backgroundColor:"red"}}>
+         <Text style ={styles.vote}>Vote</Text>
+         <Text style={styles.vote}>tokdsjfnsqkdfjndskto</Text>
+        </View>
+       
+
+      </View>
+       
+      
+
     </View>
+   
   );
 }
 
+//google map 
+// video bonde annonce + noter
+//noter le film apres visionage 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+        height:'20%',
+        width:'100%',
+        backgroundColor:"red"
+      },
+      row: {
+        flexDirection:"row",
+        flex:1
+      },
+      column: {
+        flex:1
+      },
+      image:{
+        backgroundColor:"grey",
+       height:'100%',
+       width:'20%',
+      },
+      title:{
+        flex:1,
+        paddingLeft:10,
+        fontWeight:'bold'
+      },
+      vote:{
+        flex:1,
+        paddingLeft:'70%',
+       
+      },
+      description:{
+        flex:3,
+        paddingLeft:10,
+        fontSize:11
+      },
+     
+      
 });

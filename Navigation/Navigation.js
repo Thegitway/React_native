b82df9cf989ed4d3d3d0558a26862c65218795bc
OUKIL omar <omaroukil31@gmail.com>
2021-04-16 13:38:10 +0000

    navigation 2

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,11 +1,11 @@
 
 // Navigation/Navigation.js
+import React from 'react'
+import { StyleSheet, Image } from 'react-native'
 
 import { createStackNavigator } from 'react-navigation-stack'
 import {createAppContainer} from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-
-
 import Search from '../components/Search'
 import FilmDetail from '../components/FilmDetail'
 import Favorie from '../components/Favorie'
@@ -14,24 +14,56 @@ const SearchStackNavigator = createStackNavigator({
   Search: {
     screen: Search,
     navigationOptions: {
-      title: 'Rechercher'
+      title: 'Search'
     }
   },
   FilmDetail: {
     screen: FilmDetail,
     navigationOptions: {
-      title: 'description'
+      title: 'Description'
     }
   }
 });
 
 const MoviesTabNavigator = createBottomTabNavigator({
   Search: {
-    screen: SearchStackNavigator
-  },
-  Favorites: {
-    screen: Favorie
-  }
-});
+    screen: SearchStackNavigator,
+    navigationOptions: {
 
+      tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+        return <Image
+          source={require('../assets/img/ic_search.png')}
+          style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+      }
+  }},
+ 
+ 
+  Favorites: {
+    screen: Favorie,
+    navigationOptions: {
+      title:"Favories",
+      tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+        return <Image
+          source={require('../assets/img/ic_favorite.png')}
+          style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+      }}},
+    },
+      {
+      tabBarOptions: {
+        activeBackgroundColor: 'red', // Couleur d'arrière-plan de l'onglet sélectionné
+        inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+        showLabel: false, // On masque les titres
+        showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+      }
+    }
+ 
+)
+
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
 export default createAppContainer(MoviesTabNavigator)
--- components/Favorie.js
+++ components/Favorie.js
@@ -1,17 +1,40 @@
 // Components/Favorites.js
 
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet } from 'react-native'
+import { FlatList} from 'react-native'
+import FilmItem from './FilmItem'
+import {connect} from 'react-redux'
 
 class Favorie extends React.Component {
+    
+    _displayDetailForFilm = (movie) => {
+        console.log("Display film with id "+movie.id)
+        this.props.navigation.navigate("FilmDetail", { film: movie })
+      }
 
   render() {
+
     return (
-      <Text>Mes Favoris</Text>
+        <FlatList
+        data={this.props.favoritesFilm}
+          keyExtractor={(item)=>item.id.toString()}
+        renderItem={({item}) =>(<FilmItem  
+          isFilmFavorite={(this.props.favoritesFilm.findIndex(film => film.id === item.id) !== -1) ? true : false} 
+          displayDetailForFilm={this._displayDetailForFilm} 
+          film={item} 
+        >
+        </FilmItem>
+        )}/>
     )
   }
 }
 
 const styles = StyleSheet.create({})
 
-export default Favorie
+const mapStateToProps = (state) => {
+    return {
+      favoritesFilm: state.favoritesFilm
+    }
+  }
+  export default connect(mapStateToProps)(Favorie)
